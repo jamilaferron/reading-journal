6b5b9cedee36af10687cabad7e7dd40b6c6a2ac7
jamilaferron <jamila_ferron@hotmail.co.uk>
2024-02-18 19:29:05 +0000

    Style the active link in the navbar

--- reading-journal-app/app/NavBar.tsx
+++ reading-journal-app/app/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { ImBooks } from "react-icons/im";
-
+import classnames from "classnames";
 const NavBar = () => {
+  const currentPath = usePathname();
+
   const links = [
     {
       label: "Dashboard",
@@ -23,7 +28,11 @@ const NavBar = () => {
         {links.map((link) => (
           <Link
             key={link.href}
-            className="text-cherry-blossom-pink hover:text-blush-pink transition-colors"
+            className={classnames({
+              "text-tickle-me-pink": link.href === currentPath,
+              "text-cherry-blossom-pink": link.href !== currentPath,
+              "hover:text-blush-pink transition-colors": true,
+            })}
             href={link.href}
           >
             {link.label}
---
+++ reading-journal-app/app/book-shelf/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const BookShelfPage = () => {
+  return <div>Book Shelf</div>;
+};
+
+export default BookShelfPage;
