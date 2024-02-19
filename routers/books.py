7daa7467e601e413c817259943c911d999bc0ac1
jamilaferron <jamila_ferron@hotmail.co.uk>
2024-02-19 16:57:08 +0000

    Add image upload to create Book endpoint

--- routers/books.py
+++ routers/books.py
@@ -1,13 +1,22 @@
+import os
 from typing import List
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException, status
 import schemas
 import crud
 from database import SessionLocal
+import boto3
 
 router = APIRouter(
   prefix="/books"
 )
+
+s3 = boto3.client('s3', 
+                  aws_access_key_id = os.environ['ACCESS_KEY_ID'],
+                  aws_secret_access_key = os.environ['ACCESS_SECRET_KEY']
+                  )
+
+S3_BUCKET_NAME='readingjournal-fastapi'
 
 def get_db():
   db = SessionLocal()
@@ -17,7 +26,15 @@ def get_db():
     db.close()
 
 @router.post("", status_code=status.HTTP_201_CREATED)
-def create_book(book: schemas.BookRequest, db: Session = Depends(get_db)):
+def create_book(book: schemas.BookRequest = Depends(), db: Session = Depends(get_db)):
+  # Upload file to AWS S3
+  s3.upload_fileobj(book.file.file, S3_BUCKET_NAME, book.file.filename)
+
+  uploaded_file_url = f"https://{S3_BUCKET_NAME}.s3.amazonaws.com/{book.file.filename}"
+ 
+  book.image_url = uploaded_file_url
+  
+  # Store book in database
   book = crud.create_book(db, book)
   return book
 
--- schemas.py
+++ schemas.py
@@ -1,10 +1,13 @@
+from fastapi import Form, UploadFile
 from pydantic import BaseModel
+from typing import Optional
 
 class BookRequest(BaseModel):
-    title: str
-    author: str
-    genre: str
-    image: str
+    title: str 
+    author: str 
+    genre: str 
+    file: UploadFile 
+    image_url: Optional[str] = None
     rating: float 
 
 class BookResponse(BaseModel):
